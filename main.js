dae2932977c6bc2c02ee44d75887a1d3aab8ca98
JimGav <113241613+JimGav@users.noreply.github.com>
2024-05-04 05:03:20 +0300

    implemented all, cleaned up, added analysis

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 async function bubble_sort() {
 	let start = Date.now()
 	let t = setInterval(() => document.getElementById("execution_time").innerHTML = Date.now() - start, 1)
@@ -13,7 +9,7 @@ async function bubble_sort() {
 		for (let j = length - 1; j >= k; j--) {
 
 			let div1 = document.getElementById(j)
-			let div2 = document.getElementById(j-1)
+			let div2 = document.getElementById(j - 1)
 			div1.style.backgroundColor = "#1d7484"
 			div2.style.backgroundColor = "#1d7484"
 
@@ -26,8 +22,7 @@ async function bubble_sort() {
 
 			await sleep(1000 / speed)
 
-			div1.style.backgroundColor = "#242424"
-			div2.style.backgroundColor = "#242424"
+			restore_style()
 			div1.textContent = array[j]
 			div2.textContent = array[j - 1]
 		}
@@ -36,7 +31,6 @@ async function bubble_sort() {
 	}
 	clearInterval(t)
 }
-
 
 async function selection_sort() {
 	let start = Date.now()
@@ -64,9 +58,7 @@ async function selection_sort() {
 		}
 
 		if (min_pos != curr) {
-			temp = array[min_pos]
-			array[min_pos] = array[curr]
-			array[curr] = temp
+			[array[min_pos], array[curr]] = [array[curr], array[min_pos]]
 			curr_div.textContent = array[curr]
 			document.getElementById(min_pos).textContent = array[min_pos]
 		}
@@ -77,29 +69,13 @@ async function selection_sort() {
 	clearInterval(t)
 }
 
-async function merge(left, right) {
-	let merged = [...left, ...right].sort((a, b) => a - b)
-	let start = array.indexOf(left[0])
-	for (let i = start; i < start + merged.length; i++) 
-		document.getElementById(i).style.border = "1px solid black"
-	for (let i = start; i < start + merged.length; i++) {
-		document.getElementById(i).textContent = merged[i - start]
-		array[i] = merged[i-start]
-		document.getElementById(i).style.border = "1px solid #982c61"
-	}
-	await sleep(1000 / speed)
-
-	return merged
-}
 
 async function merge_sort(a) {
 
 	if (a.length == 1)
 		return a
 
-	for (let i = 0; i < array.length; i++) {
-		document.getElementById(i).style.border = "1px solid black"
-	}
+	restore_style()
 	for (let i = 0; i < a.length; i++) {
 		document.getElementById(array.indexOf(a[i])).style.border = "1px solid #1d7484"
 	}
@@ -114,25 +90,31 @@ async function merge_sort(a) {
 	return await merge(sorted_left, sorted_right)
 }
 
-function generate_array(length) {
-	while (length != array.length) {
-		let el = Math.floor(Math.random() * 100)
-		let ptag = document.createElement("p")
-		if (!array.find(e => e == el)) {
-			array.push(el)
-			document.getElementById("array").appendChild(ptag)
-			ptag.textContent = el
-			ptag.id = array.indexOf(el)
+
+async function quickSort(low, high) {
+	if (low < high) {
+		let pi = await partition(low, high);
+		for (let i = 0; i < pi; i++) {
+			document.getElementById(i).style.border = "1px solid green"
 		}
+		document.getElementById(pi).style.backgroundColor = "#1d7484"
+		for (let i = pi + 1; i <= high; i++) {
+			document.getElementById(i).style.border = "1px solid red"
+		}
+
+		await sleep(1000 / speed)
+
+		restore_style()
+
+		await quickSort(low, pi - 1);
+		await quickSort(pi + 1, high);
 	}
 }
 
 
-function clean_up() {
-	array = []
-	document.getElementById("array").replaceChildren()
-}
 
+
+///////////////////////////////// MAIN ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Initialize
 let array = []
 const length = 10
@@ -151,8 +133,92 @@ document.getElementById("merge").onclick = async () => {
 	await merge_sort(array);
 	clearInterval(t)
 }
+document.getElementById("quick").onclick = async () => {
+	let start = Date.now()
+	let t = setInterval(() => document.getElementById("execution_time").innerHTML = Date.now() - start, 1)
+	speed = speed_el.value
+	await quickSort(0, array.length - 1)
+	clearInterval(t)
+}
 
 document.getElementById("regenerate").onclick = () => {
 	clean_up()
 	generate_array(length)
 }
+document.getElementById("bubble-more").onclick = () => { window.open("https://www.geeksforgeeks.org/bubble-sort/") }
+document.getElementById("selection-more").onclick = () => { window.open("https://www.geeksforgeeks.org/selection-sort/") }
+document.getElementById("merge-more").onclick = () => { window.open("https://www.geeksforgeeks.org/merge-sort/") }
+document.getElementById("quick-more").onclick = () => { window.open("https://www.geeksforgeeks.org/quick-sort/") }
+
+document.getElementById("code-btn").onclick = () => { window.open("https://github.com/JimGav/sorting-visualizer") }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////// UTILS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function swap_div_text(div1, div2) {
+	let temp = div1.textContent
+	div1.textContent = div2.textContent
+	div2.textContent = temp
+}
+
+function restore_style() {
+	for (let i = 0; i < array.length; i++) {
+		let div = document.getElementById(i)
+		div.style.border = "1px solid black"
+		div.style.backgroundColor = "#242424"
+	}
+}
+
+function clean_up() {
+	array = []
+	document.getElementById("array").replaceChildren()
+}
+
+async function partition(low, high) {
+	let pivot = array[high];
+	let i = low - 1;
+
+	for (let j = low; j <= high - 1; j++) {
+		if (array[j] < pivot) {
+			i++;
+			[array[i], array[j]] = [array[j], array[i]];
+			swap_div_text(document.getElementById(i), document.getElementById(j))
+		}
+	}
+	[array[i + 1], array[high]] = [array[high], array[i + 1]]; // Swap pivot to its correct position
+	swap_div_text(document.getElementById(i + 1), document.getElementById(high))
+	return i + 1;
+}
+
+
+function generate_array(length) {
+	while (length != array.length) {
+		let el = Math.floor(Math.random() * 100)
+		let ptag = document.createElement("p")
+		if (!array.find(e => e == el)) {
+			array.push(el)
+			document.getElementById("array").appendChild(ptag)
+			ptag.textContent = el
+			ptag.id = array.indexOf(el)
+		}
+	}
+}
+
+async function merge(left, right) {
+	let merged = [...left, ...right].sort((a, b) => a - b)
+	let start = array.indexOf(left[0])
+	restore_style()
+	for (let i = start; i < start + merged.length; i++) {
+		document.getElementById(i).textContent = merged[i - start]
+		array[i] = merged[i - start]
+		document.getElementById(i).style.border = "1px solid #982c61"
+	}
+	await sleep(1000 / speed)
+
+	return merged
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
